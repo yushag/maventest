ad1d4f5f03185b0d77a4e291e462e9d0b71ed4b8
yushag <yushag@users.noreply.github.com>
2017-06-11 20:06:19 +0800

    testgit

---
+++ src/main/java/com/test/test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * Created by ys on 2017/6/4.
+ */
+public class test {
+    public static void main(String []args)
+    {
+        System.out.println("hello testgit!");
+    }
+
+}
